07b0456374f683df5faf77e11a6f7258b8cfc44d
Armays <diane.lakestani@gmail.com>
2017-07-15 15:09:52 +0200

    liste des livres avec connexion au serveur mongodb stocké sur aws

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ app.engine('html', consolid.pug);
 app.set('view engine', 'html');
 app.set('views',  __dirname +  '/views');
 
-var urlDB = 'mongodb://localhost:27017/bibliotheque';
+var urlDB = 'mongodb://35.158.252.204:27017/admin';
 
 app.get('/', function(req, res) {
 	res.render("index");
 })
 .get('/books', function(req, res) {
-	app.db.collection('livre').find({}).toArray(function(err, livre) {
+	app.db.collection('bibliotheque').find({}).toArray(function(err, livre) {
+		if (err) console.log(err);
+		console.log(livre);
 		res.render("books", {
 			'livres' : livre
 		});
@@ -23,18 +25,24 @@ app.get('/', function(req, res) {
 })
 .post('/books/new', function(req, res) {
   	app.db.collection('livre').save({
-		ISBN: req.body.isbn, 
-		titre: req.body.titre, 
-		auteur: req.body.auteur, 
+		ISBN: req.body.isbn,
+		titre: req.body.titre,
+		auteur: req.body.auteur,
 		etat: req.body.etat,
-		purchase_date: new Date(req.body.dateAchat), 
-		thematique: req.body.thematique.split(",")
+		date_achat: new Date(req.body.dateAchat),
+		thematiques: req.body.thematique.split(",")
 	})
 	res.redirect('/books')
 });
 
 mongoClient.connect(urlDB, function (err, db) {
-	app.db = db;
-	app.listen(8080);
-	console.log("Express server started on 8080");
+	if (err) {
+		console.log('test');
+		console.log(err);
+	}
+	db.authenticate('admin', 'jesuisunepoule', function(err, res) {
+		app.db = db;
+		app.listen(8080);
+		console.log("Express server started on 8080");
+	});
 });
